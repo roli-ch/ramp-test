534fe67a3434c3774970ad80c64637ad9995a6ce
Roli <129250229+roli-ch@users.noreply.github.com>
2026-03-19 08:34:03 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def on_pin_pressed_p0():
-    pass
+    print(":)")
+    soll = 0
 input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+
+def on_pin_pressed_A():
+    print("A")
+    soll = 100
+input.button_is_pressed(Button.A)
 
 ist = 0
 time = input.running_time()
@@ -8,7 +14,6 @@ time = input.running_time()
 def ramp(soll, grad):
     global ist, time
     dt =  (input.running_time() - time) * grad
-    time = input.running_time()
 
     if ist < soll:
         dval = soll * dt / 1000
@@ -16,13 +21,14 @@ def ramp(soll, grad):
          #print(ist)
     else:
         ist = soll
-
     return min(ist,soll)
 
 soll = 100
 
 def on_forever():
+    global soll
+
     val = ramp(soll, 0.2)
-    print(val)
+    #print(val)
     basic.pause(200)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
 input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
-    
+    console.log(":)")
+    let soll = 0
 })
+function on_pin_pressed_A() {
+    console.log("A")
+    let soll = 100
+}
+
+input.buttonIsPressed(Button.A)
 let ist = 0
 let time = input.runningTime()
 function ramp(soll: number, grad: number): number {
     let dval: number;
     
     let dt = (input.runningTime() - time) * grad
-    time = input.runningTime()
     if (ist < soll) {
         dval = soll * dt / 1000
         ist += dval
@@ -21,7 +27,8 @@ function ramp(soll: number, grad: number): number {
 
 let soll = 100
 basic.forever(function on_forever() {
+    
     let val = ramp(soll, 0.2)
-    console.log(val)
+    // print(val)
     basic.pause(200)
 })
