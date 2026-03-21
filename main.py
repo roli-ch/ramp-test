53f8c98330e105610885d5bcb90d4a3298a511b3
Roli <129250229+roli-ch@users.noreply.github.com>
2026-03-21 11:11:01 +0100

    Aktualisiere main.ts, main.py

--- main.py
+++ main.py
@@ -1,54 +1,88 @@
+# Ramp-Test
+# =============
 
 def on_button_pressed_a():
-    global soll1
-    soll = 0
-    print("A soll: " + soll+ " ist: " + ist1)
+    global soll1, soll2
+    soll1 = 0
+    soll2 = 20
+    print("A soll: " + soll1 + " ist: " + ist1 )
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    global soll1
-    soll = 100
-    print("B soll: " + soll+ " ist: " + ist1)  
+    global soll1, soll2
+    soll1 = 100
+    soll2 = 80
+    print("B soll: " + soll1 + " ist: " + ist1 )  
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def ramp(soll, grad, kreis):
-    global ist1, time1, kreis1
+def on_logo_pressed():
+    print("Touch")
+    testA(1)
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
-    val = 0
+def ramp(soll, ist, time, grad, kreis):
+    #global ist1, time1
+    #print("ramp:"+kreis)
+    #val = 0
     #maxOk = False
     #minOk = False
     dt = (input.running_time() - time) / 1000
     time = input.running_time()
     dval = maxVal * dt * grad
+    
     if ist > soll:
         ist -= dval
-        if ist < soll:
+        if ist  < soll:
             ist = soll
-    if ist < soll:
+    if ist  < soll:
         ist += dval
-        if ist > soll:
+        if ist  > soll:
             ist = soll
-    #print("soll: " + soll + "; ist: " + ist + "; dval: " +dval+ "; dt: " + dt)
-    return ist
+    #print("Kreis" + kreis+ "; soll: " + soll + "; ist: " + ist + "; dval: " +dval+ "; dt: " + dt)
+    return ist, time
+
+def testA(nr):  
+    num = ist_array[nr]
+    #num = myA[0]
+    print(num)
 
 maxVal = 100
 minVal = 0
 
 soll1 = 100
 ist1 = 0
-kreis1 = 1
 time1 = input.running_time()
 
-def on_forever():
-    global soll1, ist1
+soll2 = 100
+ist2 = 0
+time2 = input.running_time()
 
-    val = ramp(soll1, 0.2, 1)
-    ok = (soll1 == ist1)
+ist_array = [0, 1]
+#istA = [100, 200, 300]
+#ist = 0
+
+#kreis = 1
+#timeA = [input.running_time(),input.running_time()]
+
+
+def on_forever():
+    global soll1, ist1, time1, soll2, ist2, time2
+    
+    ist1, time1 = ramp(soll1, ist1, time1, 1, 1)
+    ist2, time2 = ramp(soll2, ist2, time2, 0.5, 2)
+    #ramp(soll2, 0.5, 1)
+    #val = ramp(soll1, 0.5, 1)
+    ok = (soll1 == ist1) and (soll2 == ist2 )
     if not ok:
         pass
-        print("soll1: " + soll1 + "; ist1: " + ist1)
+        #print("soll1: " + soll1 + "; ist1: " + ist1)
+        #print("soll2: " + soll1 + "; ist2: " + ist2 )
+        print("soll1: " + soll1)
+        print("ist1: " + ist1 )
+        print("soll2: " + soll2 )
+        print("ist2: " + ist2 )
     else:
         showOk = True
     
-    basic.pause(200)
+    basic.pause(100)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,18 +1,25 @@
+//  Ramp-Test
+//  =============
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    let soll = 0
-    console.log("A soll: " + soll + " ist: " + ist1)
+    soll1 = 0
+    soll2 = 20
+    console.log("A soll: " + soll1 + " ist: " + ist1)
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
-    let soll = 100
-    console.log("B soll: " + soll + " ist: " + ist1)
+    soll1 = 100
+    soll2 = 80
+    console.log("B soll: " + soll1 + " ist: " + ist1)
 })
-function ramp(soll: number, grad: number, kreis: number): number {
-    let time: number;
-    let ist: number;
-    
-    let val = 0
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
+    console.log("Touch")
+    testA(1)
+})
+function ramp(soll: number, ist: number, time: number, grad: number, kreis: number): number[] {
+    // global ist1, time1
+    // print("ramp:"+kreis)
+    // val = 0
     // maxOk = False
     // minOk = False
     let dt = (input.runningTime() - time) / 1000
@@ -34,27 +41,52 @@ function ramp(soll: number, grad: number, kreis: number): number {
         
     }
     
-    // print("soll: " + soll + "; ist: " + ist + "; dval: " +dval+ "; dt: " + dt)
-    return ist
+    // print("Kreis" + kreis+ "; soll: " + soll + "; ist: " + ist + "; dval: " +dval+ "; dt: " + dt)
+    return [ist, time]
+}
+
+function testA(nr: number) {
+    let num = ist_array[nr]
+    // num = myA[0]
+    console.log(num)
 }
 
 let maxVal = 100
 let minVal = 0
 let soll1 = 100
 let ist1 = 0
-let kreis1 = 1
 let time1 = input.runningTime()
+let soll2 = 100
+let ist2 = 0
+let time2 = input.runningTime()
+let ist_array = [0, 1]
+// istA = [100, 200, 300]
+// ist = 0
+// kreis = 1
+// timeA = [input.running_time(),input.running_time()]
 basic.forever(function on_forever() {
     let showOk: boolean;
     
-    let val = ramp(soll1, 0.2, 1)
-    let ok = soll1 == ist1
+    let ___tempvar8 = ramp(soll1, ist1, time1, 1, 1)
+    ist1 = ___tempvar8[0]
+    time1 = ___tempvar8[1]
+    let ___tempvar9 = ramp(soll2, ist2, time2, 0.5, 2)
+    ist2 = ___tempvar9[0]
+    time2 = ___tempvar9[1]
+    // ramp(soll2, 0.5, 1)
+    // val = ramp(soll1, 0.5, 1)
+    let ok = soll1 == ist1 && soll2 == ist2
     if (!ok) {
         
-        console.log("soll1: " + soll1 + "; ist1: " + ist1)
+        // print("soll1: " + soll1 + "; ist1: " + ist1)
+        // print("soll2: " + soll1 + "; ist2: " + ist2 )
+        console.log("soll1: " + soll1)
+        console.log("ist1: " + ist1)
+        console.log("soll2: " + soll2)
+        console.log("ist2: " + ist2)
     } else {
         showOk = true
     }
     
-    basic.pause(200)
+    basic.pause(100)
 })
